f8de07478a9e5ac04361f671ba04c165a9b64af3
kimhyomin97 <rlagyalsdlek@naver.com>
2022-06-05 19:01:45 +0900

    before rollback

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,17 +2,22 @@ import { useState, useEffect } from "react";
 import http from "../api/http";
 import WordPage from "./WordPage";
 
+// // Material Kit 2 React components
+// import MKBox from "components/MKBox";
+// // Sections components
+// import BaseLayout from "layouts/sections/components/BaseLayout";
+// import View from "layouts/sections/components/View";
+// // Stats page components
+// import NavbarDark from "layouts/sections/navigation/navbars/components/NavbarDark";
+// // Stats page components code
+// import navbarDarkCode from "layouts/sections/navigation/navbars/components/NavbarDark/code";
+
 function Header() {
-  return (
-    <>
-      <header>
-        this is header
-        <a href="/login">
-          <div>login</div>
-        </a>
-      </header>
-    </>
-  );
+  // const [activeTab, setActiveTab] = useState(0);
+
+  // const handleTabType = (event, newValue) => setActiveTab(newValue);
+
+  return <div>test</div>;
 }
 
 export default Header;
--- src/component/LoginPage.js
+++ src/component/LoginPage.js
@@ -5,7 +5,7 @@ import http from "../api/http";
 function LoginPage() {
   const [loginId, setLoginId] = useState();
   const [loginPw, setLoginPw] = useState();
-
+  const [loginEmail, setLoginEmail] = useState("test@naver.com");
   const handleInputId = (e) => {
     setLoginId(e.target.value);
   };
@@ -15,9 +15,21 @@ function LoginPage() {
   };
 
   const loginClick = () => {
-    http.get("/user/login").then((res) => {
-      console.log(res);
-    });
+    http
+      .post("/user/login", {
+        userid: loginId,
+        userpw: loginPw,
+        email: loginEmail,
+      })
+      .then((res) => {
+        if (res.data == null) {
+          console.log("로그인 오류 구현");
+        } else if (res.data.userid == loginId) {
+          console.log("로그인 성공 구현");
+          // 로그인 정보 redux에 저장 or session에 저장
+          // 홈페이지 or 랜딩페이지로 페이지 이동 -> history 사용?
+        }
+      });
   };
 
   return (
@@ -37,6 +49,7 @@ function LoginPage() {
           value={loginPw}
           onChange={handleInputPw}
         ></input>
+        {/* <button type="submit">로그인</button> */}
         <button type="button" onClick={loginClick}>
           로그인
         </button>
