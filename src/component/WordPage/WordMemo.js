740fb3a7730b9ee363d15df3fcb988c3fdb6cce0
kimhyomin97 <rlagyalsdlek@naver.com>
2022-06-10 23:39:02 +0900

    word card ing

--- src/component/WordPage/WordMemo.js
+++ src/component/WordPage/WordMemo.js
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import http from "../../api/http";
+import "./public/Word.css";
 
 function WordMemo({ match }) {
   const params = useParams();
   const [word, setWord] = useState([]);
+  const [pageNum, setPageNum] = useState(0);
+
   useEffect(() => {
     // fetch 부분
     http.get(`/word/${params.target}`).then((res) => {
@@ -15,14 +18,25 @@ function WordMemo({ match }) {
 
   return (
     <>
-      <div>단어 외우기 페이지</div>
-      {word.map((item, index) => {
-        return (
-          <div key={index}>
-            {item.eng} : <input type="text" />
-          </div>
-        );
-      })}
+      {/* 카드 형식(한장씩 넘긴다) */}
+      {console.log(word[1])}
+      {/* 아래는 리스트 형식 */}
+      {/* <table>
+        <th>영어</th>
+        <th>한글</th>
+        {word.map((item, index) => {
+          return (
+            <tr key={index}>
+              <td>
+                <span className="word_eng">{item.eng}</span>
+              </td>
+              <td>
+                <input type="text" class="word_kor" />
+              </td>
+            </tr>
+          );
+        })}
+      </table> */}
     </>
   );
 }
