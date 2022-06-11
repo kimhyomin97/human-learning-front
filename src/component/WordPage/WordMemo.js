24627e34f92a564871b739883d2b691322e7c5ab
kimhyomin97 <rlagyalsdlek@naver.com>
2022-06-11 23:51:40 +0900

    memo page ing

--- src/component/WordPage/WordMemo.js
+++ src/component/WordPage/WordMemo.js
@@ -7,19 +7,43 @@ function WordMemo({ match }) {
   const params = useParams();
   const [word, setWord] = useState([]);
   const [pageNum, setPageNum] = useState(0);
+  const [visible, setVisible] = useState(false);
 
   useEffect(() => {
     // fetch 부분
-    http.get(`/word/${params.target}`).then((res) => {
+    http.get(`/word/wordlist?target=${params.target}`).then((res) => {
       //   console.log(res);
       setWord(res.data);
     });
   }, []);
 
+  const leftBtn = () => {
+    setPageNum(pageNum - 1);
+  };
+
+  const rightBtn = () => {
+    setPageNum(pageNum + 1);
+  };
+
+  const visibleBtn = () => {
+    setVisible(!visible);
+  };
+
   return (
     <>
       {/* 카드 형식(한장씩 넘긴다) */}
-      {console.log(word[1])}
+      {pageNum + 1} / {word.length}
+      <br />
+      {word[pageNum]?.eng} <br />
+      <input type="text" /> <button onClick={visibleBtn}>확인</button>
+      <br />
+      {visible == true ? word[pageNum]?.kor : <></>} <br />
+      {pageNum > 0 ? <button onClick={leftBtn}>왼쪽</button> : <></>}
+      {pageNum < word.length ? (
+        <button onClick={rightBtn}>오른쪽</button>
+      ) : (
+        <></>
+      )}
       {/* 아래는 리스트 형식 */}
       {/* <table>
         <th>영어</th>
